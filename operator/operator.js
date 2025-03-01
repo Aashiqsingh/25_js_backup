c3cfb2d80a51d24ec2d45da57b945969eb55ed1a
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-01 14:58:10 +0530

    intro

---
+++ operator/operator.js
@@ -0,0 +1,40 @@
+// var a = 13;
+// var b = 20;
+
+// // console.log("Addiiton = ",a + b);
+// console.log("Addiiton = "+(a + b));
+
+
+// console.log("value of a = "+a);
+// console.log(b/a);
+
+// console.log(b%a);
+
+// console.log(4**3);
+
+// console.log(3**4);
+
+// var a = "10";
+// var b = "10";
+
+// console.log(a == b);
+// // console.log(a != b);
+// console.log(a === b);
+
+// console.log(Math.floor(12/5));
+
+// console.log(Math.floor(9.99))
+// console.log(Math.ceil(9.01))
+// console.log(Math.round(5.49));
+// console.log(Math.random());
+// console.log(Math.floor(Math.random() * 1000));
+
+// console.log(Math.max(23,54,2,78,333,44,75));
+// console.log(Math.min(23,54,2,78,333,44,75));
+
+
+
+
+
+
+
