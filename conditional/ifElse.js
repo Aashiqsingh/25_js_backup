0dfbcb6eab6e17f291218532c4a3defa75c8a729
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-01 15:42:04 +0530

    .

---
+++ conditional/ifElse.js
@@ -0,0 +1,110 @@
+// var num = 19;
+
+
+// if(num > 0)
+// {
+//     console.log("positive");
+    
+// }
+// else{
+//     console.log("negative");
+// }
+
+
+var isAlive = false;
+var age = 19;
+
+// if(isAlive == true)
+// {
+//     if(age > 18)
+//     {
+//         console.log("eligible for vote");
+        
+//     }
+//     else{
+//         console.log("not eligible");
+        
+//     }
+// }
+// else{
+//     console.log("The person is dead");
+// }
+// if(isAlive)
+//     {
+//         if(age > 18)
+//         {
+//             console.log("eligible for vote");
+            
+//         }
+//         else{
+//             console.log("not eligible");
+            
+//         }
+//     }
+//     else{
+//         console.log("The person is dead");
+//     }
+
+
+var a = 16;
+var b = 15;
+var c = 14;
+
+
+// if(a > b && a > c)
+// {
+//     console.log(a +  " is biggest");
+    
+// }
+// else if(b > a && b > c)
+// {
+//     console.log(b + " is grettes");
+    
+// }
+// else {
+//     console.log(c + " is grettest");
+    
+// }
+
+
+// if(a > b)
+// {
+//     if(a > c)
+//     {
+//         console.log("A is bigger");
+        
+//     }
+//     else{
+//         console.log("C is bigger");
+        
+//     }
+// }
+// else{
+//     if(b > c)
+//     {
+//         console.log("B is bigger");
+        
+//     }
+//     else{
+//         console.log("c is bigger");
+        
+//     }
+// }
+
+
+
+// if()
+// {
+
+// }
+// if()
+// {
+
+// }
+// if()
+// {
+
+// }
+// else{
+
+// }
---
+++ input/input.js
@@ -0,0 +1,22 @@
+
+// var a = 10;
+// alert("Hii friends..."+a);
+
+
+// var x = confirm("Are u ready?");
+// console.log(x);
+// if(x == )
+
+
+// var x = prompt("Enter your name")
+// console.log(x);
+
+
+var a = parseInt(prompt("Enter first number :"));
+var b = parseInt(prompt("Enter second number :"));
+
+
+// console.log("Addition = ",a+b);
+// console.log(parseInt(a) + parseInt(b));
+// console.log(a+b);
+
